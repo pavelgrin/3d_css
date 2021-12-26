aceeaafa234c283479a107652c457481da0ec3da
Pavel Grinevich <pavelgrinv@gmail.com>
2021-12-26 12:17:58 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'none',
+    entry: './src/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+};
